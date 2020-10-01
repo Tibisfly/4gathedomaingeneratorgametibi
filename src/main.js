b4ce3a2c5563a9603ea9c16fd79a77dacfc521eb
Tibisfly <vielmatorotibisay@gmail.com>
2020-10-01 17:34:01 +0000

    We did it with carlos and Kike

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 require('./style.css')
 
+window.onload = function(){
+    let pronombre = ["nuestra", "ella", "la"];
+    let adjetivo = ["increible", "extraordinaria", "estupefaciente"];
+    let sustantivo = ["perra", "niña", "ornitorrinco"];
+    let dominio = [".com", ".us", ".net", ".fr"];
 
-window.load = function() {
-    console.log('Hello world')
-}
+    let i, j, k, l;
+     for (i = 0; i < pronombre.length; i++) {
+         for (j = 0; j < adjetivo.length; j++) {
+             for (k = 0; k < sustantivo.length; k++) {
+                 for(l=0; l < dominio.length; l++){
+
+                 console.log(pronombre[i] + adjetivo[j] + sustantivo[k] + dominio[l]);
+                 }
+             }
+         }
+     }
+ }
